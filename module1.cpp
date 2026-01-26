fd075a86f41fda52d6a8855db39cc42f1e425d75
ashamarien03 <ashamarien03@gmail.com>
2026-01-26 12:58:54 -0800

    Update module1.cpp

--- module1.cpp
+++ module1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Print greeting
+    cout << "Hello Inventory System!" << endl;
+
+    // Demonstrate escape characters
+    cout << "This is on a new line.\n";
+    cout << "This line is tabbed in.\tSee?" << endl;
+
+    // Output sizes of data types
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+
+    return 0;
+}
 
